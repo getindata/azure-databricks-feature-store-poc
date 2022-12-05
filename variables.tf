602e4000b0b5bb07788881d16f7c3b35d667f7fa
Szymon Zaczek <szymon.zaczek@getindata.com>
2022-12-05 14:05:23 +0100

    Removind redundant resources

--- variables.tf
+++ variables.tf
@@ -13,37 +13,37 @@ variable "resource_group" {
 variable "stg_name" {
   type        = string
   description = "Name of the storage to be deployed"
-  default     = "gidadls2featurestorepoc"
+  default     = "gidadls2featurestorepoc3"
 }
 
 variable "dbx_workspace_name" {
   type        = string
   description = "Name of Databricks workspace"
-  default     = "giddbxfeaturestorepoc"
+  default     = "giddbxfeaturestorepoc3"
 }
 
 variable "kv_name" {
   type        = string
   description = "Name of the Key Vault"
-  default     = "gid-kv-featurestore-poc"
+  default     = "gid-kv-featurestore-poc3"
 }
 
 variable "app_configuration_name" {
   type        = string
   description = "Name of the App Configuration"
-  default     = "gidappconffeaturestorepoc"
+  default     = "gidappconffeaturestorepoc3"
 }
 
 variable "cosmos_db_name" {
   type        = string
   description = "Name of the Cosmos DB Workspace"
-  default     = "gid-cosmos-db-acc-featurestore-poc"
+  default     = "gid-cosmos-db-acc-featurestore-poc3"
 }
 
 variable "log_workspace_name" {
   type        = string
   description = "Name of the Log Analytics Workspace"
-  default     = "gid-log-featurestore-poc"
+  default     = "gid-log-featurestore-poc3"
 }
 
 variable "cluster_name" {
@@ -62,10 +62,4 @@ variable "notebook_2" {
   type        = string
   description = "Notebook 2 name"
   default     = "taxi_notebook.py"
-}
-
-variable "notebook_3" {
-  type        = string
-  description = "Notebook 3 name"
-  default     = "offline_online_sync.py"
 }
